22468f34ec74507edc46a66967f3710c016d8df4
Selina <selinachang@gmail.com>
2014-02-25 21:24:47 -0800

    Added routing, a node web server, some styling for landing page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ebullientious.github.io
 =======================
+
+Run node ./js/web-server.js to get localhost up.
--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 // script.js
 
-  // create the module and name it scotchApp
-  var weddingApp = angular.module('weddingApp', []);
+// create the module and name it scotchApp
+var weddingApp = angular.module('weddingApp', ['ngRoute']);
 
-  // create the controller and inject Angular's $scope
-  weddingApp.controller('mainController', function($scope) {
+weddingApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/story', {
+      templateUrl : 'html/story.html',
+      controller : 'storyController'
+    })
+    .when('/rsvp', {
+      templateUrl : 'html/rsvp.html',
+      controller : 'rsvpController'
+    })
+    .when('/venue', {
+      templateUrl : 'html/venue.html',
+      controller : 'venueController'
+    })
+    .when('/map', {
+      templateUrl : 'html/map.html',
+      controller : 'mapController'
+    })
+    .otherwise({
+      templateUrl : 'html/landing.html',
+      controller : 'landingController'
+    })
+    // .otherwise({
+    //   redirectTo : '/'
+    // });
+});
 
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-  });
+// create the controller and inject Angular's $scope
+weddingApp.controller('mainController', function($scope) {
+});
+
+weddingApp.controller('landingController', function($scope) {
+});
+
+weddingApp.controller('mapController', function($scope) {
+});
+
+weddingApp.controller('venueController', function($scope) {
+});
+
+weddingApp.controller('storyController', function($scope) {
+});
+
+weddingApp.controller('rsvpController', function($scope) {
+});
