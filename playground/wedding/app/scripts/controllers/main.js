036dd52d9f841abe5bbc4241d666dad528f42063
Selina <selina@Selinas-MacBook-Pro.local>
2014-07-23 00:24:25 -0700

    Putting src of wedding in "playground/wedding"
    Eventually put “playgorund/wedding/app/dist” contents in “wedding”

---
+++ playground/wedding/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weddingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weddingApp
+ */
+angular.module('weddingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
